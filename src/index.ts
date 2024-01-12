fb302d8c65f2755a51df2a9a84c7a1e3dcaae30a
Abir <abirmahmud221@gmail.com>
2024-01-12 11:33:38 +0600

    functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-const enum Size { Small =1, Medium, Large };
-let mySize: Size = Size.Medium;
-console.log(mySize);
+function calculateTax(income: number, taxYear = 2022): number {
+
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
+}
+
+calculateTax(10_000);
